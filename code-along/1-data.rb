aa90754b639cc50f4826e72239e12326103279c5
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-01-27 01:00:07 +0000

    Note update

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,14 +5,33 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3
+puts 5
 
 # Perform simple math with numbers
+puts 5 + 2
+puts 5 - 2 
+puts 5 * 2
+puts 5 / 2
+
+#Integers vs Floats 
+puts 5.0 / 2.0
+
+#Order of Operations
+puts (2 + 5) * 5
+#use paranthensis
 
 # Strings
+puts "Hello, world!"
 
 # Combine strings together
+puts "Tacos are " + "delish"
+puts "tacos" * 3
+puts "tacos" + 3.to_s
+puts "tacos" + "3"
 
 # Variables
+
 
 # Combine strings and variables
 
