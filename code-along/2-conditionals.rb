3d4b45a009e66fdea3e4d544ed9993a3c9a89e01
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-01-27 01:29:52 +0000

    conditionals part 1

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -2,9 +2,19 @@
 # is the same as where this file is located and then run:
 # ruby 2-conditionals.rb
 
-# Booleans
+# Booleans - True or False
+test_is_true = true 
+puts test_is_true
+
+test_is_false = false
+puts test_is_false
 
 # Boolean Expressions
+puts 3 == 2 #this expression is false so returns false, use a double equals (comparison operator). Using only one equals can cause issues 
+puts 3 !=2 #!= means not equals to
+puts 3 > 2
+puts 3 < 2
+
 
 # If Conditional Logic
 
